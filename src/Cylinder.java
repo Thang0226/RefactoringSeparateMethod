064b77ca7d4e525f5762be4c5f9ba734ebd50671
Thang0226 <thang.nd0226@gmail.com>
2024-10-18 15:57:08 +0700

    separate getBaseArea and getPerimeter methods

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,8 +1,14 @@
 public class Cylinder {
-    public static double getVolume(int radius, int height){
-        double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
-        double volume = perimeter * height + 2 * baseArea;
-        return volume;
+    public static double getArea(int radius, int height)
+        double area = getPerimeter(radius) * height + 2 * getBaseArea(radius);
+        return area;
+    }
+
+    public static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
     }
 }
